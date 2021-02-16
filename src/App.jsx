cc8fcf5b01c49a8c2064f875b20ee45f7d64fedf
lowell1 <29132034+lowell1@users.noreply.github.com>
2021-02-16 15:05:09 -0800

    material ui navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,41 @@
+import "./meyer_reset.css";
 import React from "react";
 import Nav from "./Nav";
 import Home from "./Home";
 import Projects from "./Projects";
 import Contact from "./Contact";
 import { BrowserRouter, Route } from "react-router-dom";
-import "./app.css";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#000",
+    },
+    text: {
+      primary: "#FFFFFF",
+    },
+  },
+});
 export default () => {
   return (
     <>
-      <BrowserRouter>
-        <Nav />
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/projects">
-          <Projects />
-        </Route>
-        <Route path="/contact">
-          <Contact />
-        </Route>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Nav />
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </BrowserRouter>
+      </ThemeProvider>
     </>
   );
 };
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,11 +1,45 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Drawer, Link } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    // backgroundColor: "black",
+    // "& > * > *": {
+    //   color: "white",
+    // },
+    "& > * > * + *": {
+      marginLeft: "1rem",
+    },
+  },
+});
 
 export default () => {
+  const classes = useStyles();
+
   return (
-    <>
-      <p className="md:hidden">welfej</p>
-      <p className="hidden lg:block">oohhaaahhh!!!</p>
-    </>
+    <AppBar className={classes.root} position="static">
+      <Toolbar>
+        <Link color="textPrimary" href="/">
+          Home
+        </Link>
+        <Link color="textPrimary" href="/projects">
+          Projects
+        </Link>
+        <Link color="textPrimary" href="/contact">
+          Contact
+        </Link>
+      </Toolbar>
+    </AppBar>
   );
 };
+
+/**
+ <Drawer anchor="top" open={true}>
+        <Link href="/">home</Link>
+        {/* <MuiLink component={Link} to="/projects">
+          projects
+        </MuiLink> 
+        <Link href="/projects">projects</Link>
+      </Drawer>
+ */
