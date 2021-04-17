084549b85b0e1d627202404d26f281dc96d74cfa
lowell <>
2021-04-16 19:00:09 -0700

    change 'to' property in mailOptions to be the same email in env variable

--- pages/api/send_email.js
+++ pages/api/send_email.js
@@ -4,10 +4,9 @@ const nodemailer = require("nodemailer");
 
 export default (req, res) => {
   const transporter = nodemailer.createTransport({
-    // host: process.env.SMTP_HOST,
-    // port: process.env.SMTP_PORT,
-    // secure: process.env.SMTP_PORT === 465,
-    service: "sendgrid",
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: process.env.SMTP_PORT === 465,
     auth: {
       user: process.env.SMTP_USERNAME,
       pass: process.env.SMTP_PASSWORD,
@@ -16,13 +15,16 @@ export default (req, res) => {
 
   transporter
     .sendMail({
-      from: req.body.email,
-      to: process.env.SMTP_ADDRESS,
-      subject: "lol",
-      text: req.body.message,
+      from: process.env.SMTP_EMAIL_ADDRESS,
+      to: process.env.SMTP_EMAIL_ADDRESS,
+      subject: "Contact Form",
+      text: `${req.body.message}\n\n from ${req.body.email}`,
     })
     .then(() => res.status(200).json({ message: "Message received!" }))
-    .catch(() =>
-      res.status(500).json({ message: "Sorry, your message could not be sent" })
-    );
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ message: "Sorry, your message could not be sent" });
+    });
 };
